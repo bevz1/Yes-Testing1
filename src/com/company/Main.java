12a2b616784b3361fbf786016b82affeb1f4f014
bevz1 <bevanrichards@yahoo.com>
2021-02-17 02:39:03 +0200

    Hello world and yes print - Now check

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Yes");
+        System.out.println("Check");
 	// write your code here
     }
 }
